5a86925661b51d0eb2ae9af4889673bd5da884e4
AhmedHussain007 <147406446+AhmedHussain007@users.noreply.github.com>
2025-04-09 12:47:46 +0500

    mid lab exam

--- MID LAB EXAM/frontend/scripts/script.js
+++ MID LAB EXAM/frontend/scripts/script.js
@@ -1,16 +1,19 @@
 const displaynewin = () =>{
+  document.querySelector('.labmenu').style.display = 'none';
   document.querySelector('.newin').style.display = 'block';
 }
 const hidenewin = () =>{
   document.querySelector('.newin').style.display = 'none';
 }
 const displayclothing = () =>{
+  document.querySelector('.labmenu').style.display = 'none';
   document.querySelector('.clothing').style.display = 'block';
 }
 const hideclothing = () =>{
   document.querySelector('.clothing').style.display = 'none';
 }
 const displaydresses = () =>{
+  document.querySelector('.labmenu').style.display = 'none';
   document.querySelector('.dresses').style.display = 'block';
 }
 const hidedresses = () =>{
